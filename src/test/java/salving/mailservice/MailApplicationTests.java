b237ccc0e328170dda243cc1d5a07f5e66f9a027
Salving <Salving222@yandex.ru>
2023-08-16 23:59:57 +0700

    add test database

--- src/test/java/salving/mailservice/MailApplicationTests.java
+++ src/test/java/salving/mailservice/MailApplicationTests.java
@@ -1,7 +1,12 @@
 package salving.mailservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 class MailApplicationTests {
